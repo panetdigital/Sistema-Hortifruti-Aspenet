87eb454cef0e9b44c92dc3b9830c9724121f2c8e
panet <panetagenciadigital@gmail.com>
2024-07-18 00:01:56 -0300

    Initial commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using NovoProjetoFrutas.Model;
+
+namespace NovoProjetoFrutas.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Adicionando dados de semente
+            modelBuilder.Entity<Produto>().HasData(
+                new Produto
+
+                {
+                    Id = 1,
+                    Cod = "192",
+                    CodItem = "1922",
+                    Nome = "Abóbora Moranga",
+                    Preco = 5,
+                    PrecoPromo = 4,
+                    Grupo = "legume",
+                    EstoqueDisponivel = 100,
+                    Link = "\"https:\\/\\/www.nagumo.com.br\\/sao-paulo-lj17-pires-do-rio-jardim-lideranca-avenida-pires-do-rio\\/produto\\/abobora-------------------------moranga",
+                    Imagem = "\"https:\\/\\/www.nagumo.com.br\\/sao-paulo-lj17-pires-do-rio-jardim-lideranca-avenida-pires-do-rio\\/produto\\/abobora-------------------------moranga"
+                }
+            );
+        }
+        public DbSet<NovoProjetoFrutas.Model.Produto> Produto { get; set; } = default!;
+    }
+}
---
+++ Model/Produto.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NovoProjetoFrutas.Model
+{
+    public class Produto
+    {
+        [Key]  // Define a propriedade Id como a chave primária
+        public int Id { get; set; }
+
+        [Required]  // Define que a propriedade é obrigatória
+        public string Cod { get; set; }
+
+        [Required]
+        public string CodItem { get; set; }
+
+        [Required]
+        [MaxLength(100)]  // Define o tamanho máximo da string
+        public string Nome { get; set; }
+
+        [DataType(DataType.Currency)]  // Define o tipo de dado como moeda
+        public decimal? Preco { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal? PrecoPromo { get; set; }  // Propriedade nullable para promoções
+
+        [MaxLength(50)]
+        public string Grupo { get; set; }
+
+  
+        public int EstoqueDisponivel { get; set; }
+
+        [Url]  // Define que a propriedade deve ser um URL válido
+        public string Link { get; set; }
+
+        [Url]
+        public string Imagem { get; set; }
+    }
+}
